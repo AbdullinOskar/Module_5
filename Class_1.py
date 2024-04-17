ac1e994ae3acca5ebb3bac9a10f0c7ba885f26e8
Oskar <106113902+AbdullinOskar@users.noreply.github.com>
2024-04-17 19:50:32 +0300

    Add files via upload

--- Class_1.py
+++ Class_1.py
@@ -1,14 +1,24 @@
-class House:
+    # Ваша задача:
+    # Создайте новый класс House
+    # Задайте ему новый атрибут numberOfFloors = 10
+    # В цикле пройдитесь по атрибуту numberOfFloors и распечайте значение "Текущий этаж равен"
+class House():
 
     def __init__(self):
-        self.floor = 0
+        self.numberOfFloors = 10
 
-    def numberOfFloors(self):
-        self.floor = 10
+    def elevator(self):
+        for floor in range(1, self.numberOfFloors + 1):
+            print('Текущий этаж равен =', floor)
 
-
+print(House().numberOfFloors)
 my_house = House()
-my_house.numberOfFloors()
-print('Текущий этаж равен ', my_house.floor)
+print(my_house.numberOfFloors)
 
+for floor in range(1, my_house.numberOfFloors + 1):
+    print('Текущий этаж равен', floor)
+    
+print()
+
+my_house.elevator()
 
